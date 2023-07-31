e27e5678fe5b023f9a4f7edfb7cc2969b0fe5207
Samantha Hurtado <samanthahurtado@samanthas-mbp.lan>
2023-07-31 11:59:53 -0700

    beginning of form component create trip

--- src/components/CreateTrip/index.js
+++ src/components/CreateTrip/index.js
@@ -4,6 +4,22 @@ function CreateTrip() {
   return (
     <div>
       <div className='page-title'>Create Trip</div>
+      <form>
+        <label htmlFor='trip-name'>Trip Name</label>
+          <input>
+            required
+            type='text'
+            id='trip-name'
+            name='trip-name'
+          </input>
+        <label htmlFor='trip-description'>Trip Description</label>
+          <input>
+              required
+              type='text'
+              id='trip-description'
+              name='trip-description'
+            </input>
+      </form>
     </div>
   );
 }
